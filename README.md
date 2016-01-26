6b8597dc482c7bbbb3216a38baea1a36df15a3f8
Nil Portugués Calderó <nilopc@gmail.com>
2016-01-27 00:21:30 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,85 @@
-# php-repository-cache
-Repository with Cache layer using StashPHP
+# Repository Cache
+
+Repository to be used with **[nilportugues/repository](https://github.com/nilportugues/php-repository)** implementing a cache layer using [StashPHP](http://www.stashphp.com/). 
+
+## Installation
+
+Use [Composer](https://getcomposer.org) to install the package:
+
+```json
+$ composer require nilportugues/repository-cache
+```
+
+## Usage
+
+```php
+use NilPortugues\Foundation\Infrastructure\Model\Repository\Cache\RepositoryCache;
+use Stash\Driver\Ephemeral;
+use Stash\Driver\Memcache;
+use Stash\Pool;
+
+$memcached = new Memcache();
+$memcached->setOptions(array('servers' => array('127.0.0.1', '11211')));
+
+$cachePool = new Pool();
+$cachePool->setDriver(new Ephemeral());
+$cachePool->setDriver($memcached);
+
+/**
+ * MySQLColorRepository is an implementation of:
+ *  - NilPortugues\Foundation\Domain\Model\Repository\Contracts\PageRepository
+ *  - NilPortugues\Foundation\Domain\Model\Repository\Contracts\ReadRepository
+ *  - NilPortugues\Foundation\Domain\Model\Repository\Contracts\WriteRepository
+ */
+$repository = new MySQLColorRepository($data);
+$cacheRepository = new RepositoryCache($cachePool, $repository, Color::class, '3600');
+
+/**
+ * Color Repository is a wrapper class implementing the same interfaces
+ * as the previously defined MySQLColorRepository.
+ */
+$repository = new ColorRepository($cacheRepository);
+
+//Now use as normal... 
+
+$color = new Color('#@@@@@@', 'New color');
+$repository->add($color);
+
+$repository->find(ColorId('#@@@@@@')); //should hit cache and return an instance of Color.
+```
+
+
+
+## Quality
+
+To run the PHPUnit tests at the command line, go to the tests directory and issue phpunit.
+
+This library attempts to comply with [PSR-1](http://www.php-fig.org/psr/psr-1/), [PSR-2](http://www.php-fig.org/psr/psr-2/), [PSR-4](http://www.php-fig.org/psr/psr-4/).
+
+If you notice compliance oversights, please send a patch via [Pull Request](https://github.com/nilportugues/php-repository-cache/pulls).
+
+
+## Contribute
+
+Contributions to the package are always welcome!
+
+* Report any bugs or issues you find on the [issue tracker](https://github.com/nilportugues/php-repository-cache/issues/new).
+* You can grab the source code at the package's [Git repository](https://github.com/nilportugues/repository).
+
+
+## Support
+
+Get in touch with me using one of the following means:
+
+ - Emailing me at <contact@nilportugues.com>
+ - Opening an [Issue](https://github.com/nilportugues/php-repository-cache/issues/new)
+
+
+## Authors
+
+* [Nil Portugués Calderó](http://nilportugues.com)
+* [The Community Contributors](https://github.com/nilportugues/php-repository-cache/graphs/contributors)
+
+
+## License
+The code base is licensed under the [MIT license](LICENSE).
---
+++ src/RepositoryCache.php
@@ -0,0 +1,219 @@
+<?php
+
+/**
+ * Author: Nil Portugués Calderó <contact@nilportugues.com>
+ * Date: 26/01/16
+ * Time: 23:55.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace NilPortugues\Foundation\Infrastructure\Model\Repository\Cache;
+
+use NilPortugues\Foundation\Domain\Model\Repository\Contracts\Fields;
+use NilPortugues\Foundation\Domain\Model\Repository\Contracts\Filter;
+use NilPortugues\Foundation\Domain\Model\Repository\Contracts\Identity;
+use NilPortugues\Foundation\Domain\Model\Repository\Contracts\Pageable;
+use NilPortugues\Foundation\Domain\Model\Repository\Contracts\PageRepository;
+use NilPortugues\Foundation\Domain\Model\Repository\Contracts\ReadRepository;
+use NilPortugues\Foundation\Domain\Model\Repository\Contracts\Sort;
+use NilPortugues\Foundation\Domain\Model\Repository\Contracts\WriteRepository;
+use Stash\Interfaces\PoolInterface;
+
+class RepositoryCache implements ReadRepository, WriteRepository, PageRepository
+{
+    /**
+     * @var PoolInterface
+     */
+    protected $cache;
+    /**
+     * @var ReadRepository|WriteRepository|PageRepository
+     */
+    protected $repository;
+    /**
+     * @var string
+     */
+    protected $cacheNamespace;
+    /**
+     * @var string
+     */
+    protected $cacheNamespaceFindBy;
+    /**
+     * @var string
+     */
+    protected $cacheNamespacePage;
+    /**
+     * @var string
+     */
+    protected $cacheNamespaceCount;
+
+    /**
+     * @var null
+     */
+    protected $cacheTime = null;
+
+    /**
+     * RepositoryCache constructor.
+     *
+     * @param PoolInterface $cache
+     * @param mixed         $repository
+     * @param string        $classFQN
+     * @param null          $cacheTime
+     */
+    public function __construct(PoolInterface $cache, $repository, $classFQN, $cacheTime = null)
+    {
+        $this->cache = $cache;
+        $this->repository = $repository;
+        $this->cacheTime = ($cacheTime) ? strtotime(sprintf('now + %s seconds', $cacheTime)) : $cacheTime;
+
+        $this->buildCacheKeys($classFQN);
+    }
+
+    /**
+     * Creates the cache key for the repository.
+     *
+     * @param string $classFQN
+     */
+    protected function buildCacheKeys($classFQN)
+    {
+        $parts = explode('\\', $classFQN);
+        $classFQN = array_pop($parts);
+
+        $baseKey = str_replace('\\', '.', strtolower($classFQN));
+        $this->cacheNamespace = sprintf('/%s/', $baseKey);
+        $this->cacheNamespaceFindBy = sprintf('/%s/findby/', $baseKey);
+        $this->cacheNamespacePage = sprintf('/%s/paginated/', $baseKey);
+        $this->cacheNamespaceCount = sprintf('/%s/count/', $baseKey);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function find(Identity $id, Fields $fields = null)
+    {
+        $key = $this->cacheNamespace.$id->id();
+        $cachedItem = $this->cache->getItem($key);
+
+        if (!$cachedItem->isMiss() && null !== ($result = $cachedItem->get())) {
+            return $result;
+        }
+
+        $entity = $this->repository->find($id, $fields);
+        $cachedItem->set($entity, $this->cacheTime);
+
+        return $entity;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function findBy(Filter $filter = null, Sort $sort = null, Fields $fields = null)
+    {
+        $cachedItem = $this->cache->getItem(
+            $this->cacheNamespaceFindBy.md5(serialize($filter).serialize($sort).serialize($fields))
+        );
+
+        if (!$cachedItem->isMiss() && null !== ($result = $cachedItem->get())) {
+            return $result;
+        }
+
+        $result = $this->repository->findBy($filter, $sort, $fields);
+        $cachedItem->set($result, $this->cacheTime);
+
+        return $result;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function add(Identity $value)
+    {
+        $entity = $this->repository->add($value);
+
+        $cachedItem = $this->cache->getItem($this->cacheNamespace.$entity->id());
+        $cachedItem->set($entity, $this->cacheTime);
+
+        return $entity;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function remove(Identity $id)
+    {
+        $result = $this->repository->remove($id);
+
+        $this->cache->getItem($this->cacheNamespace.$id->id())->clear();
+        $this->cache->getItem($this->cacheNamespacePage)->clear();
+        $this->cache->getItem($this->cacheNamespaceFindBy)->clear();
+        $this->cache->getItem($this->cacheNamespaceCount)->clear();
+
+        return $result;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function findAll(Pageable $pageable = null)
+    {
+        $cachedItem = $this->cache->getItem($this->cacheNamespacePage.md5(serialize($pageable)));
+
+        if (!$cachedItem->isMiss() && null !== ($result = $cachedItem->get())) {
+            return $result;
+        }
+
+        $result = $this->repository->findAll($pageable);
+        $cachedItem->set($result, $this->cacheTime);
+
+        return $result;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function count(Filter $filter = null)
+    {
+        $cachedItem = $this->cache->getItem($this->cacheNamespaceCount.md5(serialize($filter)));
+
+        if (!$cachedItem->isMiss() && null !== ($result = $cachedItem->get())) {
+            return $result;
+        }
+
+        $result = $this->repository->count($filter);
+        $cachedItem->set($result, $this->cacheTime);
+
+        return $result;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function exists(Identity $id)
+    {
+        return $this->repository->exists($id);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function addAll(array $values)
+    {
+        $result = $this->repository->addAll($values);
+
+        $this->cache->getItem($this->cacheNamespace)->clear();
+
+        return $result;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function removeAll(Filter $filter = null)
+    {
+        $result = $this->repository->removeAll($filter);
+
+        $this->cache->getItem($this->cacheNamespace)->clear();
+
+        return $result;
+    }
+}
